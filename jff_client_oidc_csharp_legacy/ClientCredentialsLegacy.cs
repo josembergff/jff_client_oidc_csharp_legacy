90e6e7914303c0a9c3f67adc469810eb8744b267
Josemberg Ferreira <josembergff@gmail.com>
2024-01-23 14:29:46 -0300

    Replace version .net

--- jff_client_oidc_csharp_legacy/ClientCredentialsLegacy.cs
+++ jff_client_oidc_csharp_legacy/ClientCredentialsLegacy.cs
@@ -4,10 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.UI;
 
 namespace jff_client_oidc_csharp_legacy
 {
--- jff_client_oidc_csharp_legacy/Properties/AssemblyInfo.cs
+++ jff_client_oidc_csharp_legacy/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Client OIDC C# Legacy")]
-[assembly: AssemblyDescription("Simple client for request OIDC token and requests for legacy .net")]
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("jff_client_oidc_csharp_legacy")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Josemberg Farias Ferreira")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("jff_client_oidc_csharp_legacy")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8d1e14b2-9a96-4295-910e-4cccd9c7fef2")]
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("805ec4ba-f031-4174-a0c5-9972c53ff4b4")]
 
-// Version information for an assembly consists of the following four values:
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
