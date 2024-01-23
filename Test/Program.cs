a0b1f3283f6c1aadc0cee604d85ee5f3c778672b
Josemberg Ferreira <josembergff@gmail.com>
2024-01-23 14:35:41 -0300

    Adjust test

---
+++ Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using jff_client_oidc_csharp_legacy;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            testeClientLegacy();
+
+            Console.ReadLine();
+        }
+
+        private static void testeClientLegacy()
+        {
+            var client = new ClientCredentialsLegacy("https://localhost:62862", "api", "secret", new string[] { "openid" });
+
+            var token = client.GetToken();
+
+            if (token.Success)
+            {
+                var resultAPI = client.Get<dynamic>("https://localhost:3000", "/products");
+
+                Console.WriteLine(resultAPI.Result.ToString());
+            }
+            else
+            {
+                Console.WriteLine(token.Error);
+                Console.WriteLine(string.Join(";", token.ListErrors));
+            }
+        }
+    }
+}
